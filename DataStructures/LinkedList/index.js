6c9c315709ac4869042b28cdfde7ece4522f9fc8
jonathanferreras <jonathanferreras@icloud.com>
2019-10-27 22:26:38 -0400

    linkedlist: started

---
+++ DataStructures/LinkedList/index.js
@@ -0,0 +1,21 @@
+// --- Directions
+// Implement classes Node and Linked Lists
+// See 'directions' document
+
+/**
+ * My initial implementation
+ */
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {}
+
+module.exports = {
+  Node,
+  LinkedList
+}
