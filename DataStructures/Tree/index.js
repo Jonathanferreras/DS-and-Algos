c7e796792e716837f58d3edbf6a5d89dbaf449fa
jonathanferreras <jonathanferreras@icloud.com>
2019-11-04 19:49:28 -0500

    tree: impl for traverse depth-first

--- DataStructures/Tree/index.js
+++ DataStructures/Tree/index.js
@@ -49,7 +49,16 @@ class Tree {
     }
   }
 
-  traverseDF(fn) {}
+  traverseDF(fn) {
+    const arr = [this.root];
+
+    while(arr.length) {
+      const node = arr.shift();
+
+      arr.unshift(...node.children);
+      fn(node);
+    }
+  }
 }
 
 module.exports = {
