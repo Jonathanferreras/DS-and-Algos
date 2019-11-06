851a3783428ef0448b42c0dfeba17e07bfc92f6a
jonathanferreras <jonathan.ferreras@nyct.com>
2019-11-06 14:50:26 -0500

    selectionSort: my impl

---
+++ Exercises/SelectionSort/index.js
@@ -0,0 +1,28 @@
+// --- Directions
+// Implement selectionSort
+
+/**
+ * My initial implementation
+ */
+
+
+module.exports = function selectionSort(arr) {
+  for(let i = 0; i < arr.length; i++) {
+    let min = i;
+
+    for(let j = i + 1; j < arr.length; j++) {
+      if(arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+
+    if(min !== i) {
+      let temp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = temp;
+    }
+
+  }
+
+  return arr;
+}
